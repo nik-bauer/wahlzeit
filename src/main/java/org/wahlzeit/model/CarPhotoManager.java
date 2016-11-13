49682f5a7c703402932bb3a1f5ae96c25dcf16e1
Jan Niklas Bauer <jan.niklas.bauer@fau.de>
2016-11-13 22:24:47 +0100

    added CarPhotoManager class

---
+++ src/main/java/org/wahlzeit/model/CarPhotoManager.java
@@ -0,0 +1,50 @@
+package org.wahlzeit.model;
+
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+
+public class CarPhotoManager extends PhotoManager {
+
+    /**
+     *
+     */
+    protected static final PhotoManager instance = new CarPhotoManager();
+
+    private static final Logger log = Logger.getLogger(CarPhotoManager.class.getName());
+
+    /**
+     *
+     * @param classification
+     * @return Photos with the given classification.
+     */
+    public Set<CarPhoto> findByClassification(CarClassification classification) {
+        Set<CarPhoto> result = new HashSet<>();
+
+        readObjects(result, CarPhoto.class, CarPhoto.CLASSIFICATION, classification);
+
+        for (CarPhoto carPhoto : result) {
+            doAddPhoto(carPhoto);
+        }
+
+        return result;
+    }
+
+    /**
+     *
+     * @param manufacturer
+     * @return Photos from the given manufacturer.
+     */
+    public Set<CarPhoto> findByManufacturer(CarManufacturer manufacturer) {
+        Set<CarPhoto> result = new HashSet<>();
+
+        readObjects(result, CarPhoto.class, CarPhoto.MANUFACTURER, manufacturer);
+
+        for (CarPhoto carPhoto : result) {
+            doAddPhoto(carPhoto);
+        }
+
+        return result;
+    }
+}
