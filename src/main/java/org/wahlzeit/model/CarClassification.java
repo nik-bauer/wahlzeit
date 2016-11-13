431b1e811b57ec87f69c1380d837e7450312f3fe
Jan Niklas Bauer <jan.niklas.bauer@fau.de>
2016-11-13 22:23:14 +0100

    added new CarPhoto class and corresponding enums

---
+++ src/main/java/org/wahlzeit/model/CarClassification.java
@@ -0,0 +1,18 @@
+package org.wahlzeit.model;
+
+
+public enum CarClassification {
+
+    // TODO: not sure if this covers all types of cars
+    MICRO_CAR,
+    COMPACT_CAR,
+    FULLSIZE_CAR,
+    GRAND_TOURER,
+    SUPERCAR,
+    ROADSTER,
+    CONVERTIBLE,
+    MINIVAN,
+    SUV,
+    PICKUP_TRUCK
+
+}
---
+++ src/main/java/org/wahlzeit/model/CarPhoto.java
@@ -0,0 +1,164 @@
+package org.wahlzeit.model;
+
+
+import com.googlecode.objectify.annotation.Entity;
+
+@Entity
+public class CarPhoto extends Photo {
+
+    public static final String CLASSIFICATION = "classification";
+    public static final String MANUFACTURER = "manufacturer";
+
+    // TODO: maybe add color in case the actual car on the photo should be described instead of the car in general
+    private CarManufacturer manufacturer;
+    private CarClassification classification;
+    private int numDoors;
+    private int numSeats;
+    private int engineHP;
+    private float engineDisplacementInLiters;
+    private float carLength;
+    private float carWidth;
+
+    /**
+     * @methodtype constructor
+     */
+    public CarPhoto() {
+        super();
+    }
+
+    /**
+     * @methodtype constructor
+     */
+    public CarPhoto(PhotoId id) {
+        super(id);
+    }
+
+    /**
+     * @methodtype constructor
+     */
+    public CarPhoto(PhotoId id, CarManufacturer manufacturer, CarClassification classification, int numDoors, int numSeats, int engineHP, float engineDisplacementInLiters, float carLength, float carWidth) {
+        super(id);
+        this.manufacturer = manufacturer;
+        this.classification = classification;
+        this.numDoors = numDoors;
+        this.numSeats = numSeats;
+        this.engineHP = engineHP;
+        this.engineDisplacementInLiters = engineDisplacementInLiters;
+        this.carLength = carLength;
+        this.carWidth = carWidth;
+    }
+
+    /**
+     *
+     * @methodtype set
+     * @param classification
+     */
+    public void setClassification(CarClassification classification) {
+        this.classification = classification;
+    }
+
+    /**
+     *
+     * @methodtype get
+     * @return Classification of the car.
+     */
+    public CarClassification getClassification() {
+        return this.classification;
+    }
+
+    /**
+     * @methodtype set
+     * @param numDoors
+     */
+    public void setNumDoors(int numDoors) {
+        this.numDoors = numDoors;
+    }
+
+    /**
+     * @methodtype get
+     * @return Number of the car's doors.
+     */
+    public int getNumDoors() {
+        return this.numDoors;
+    }
+
+    /**
+     * @methodtype set
+     * @param numSeats
+     */
+    public void setNumSeats(int numSeats) {
+        this.numSeats = numSeats;
+    }
+
+    /**
+     * @methodtype get
+     * @return Number of the car's seats.
+     */
+    public int getNumSeats() {
+        return this.numSeats;
+    }
+
+    /**
+     * @methodtype set
+     * @param engineHP
+     */
+    public void setEngineHP(int engineHP) {
+        this.engineHP = engineHP;
+    }
+
+    /**
+     * @methodtype get
+     * @return Total horsepower of the car's engine.
+     */
+    public int getEngineHP() {
+        return this.engineHP;
+    }
+
+    /**
+     * @methodtype set
+     * @param engineDisplacementInLiters
+     */
+    public void setEngineDisplacementInLiters(int engineDisplacementInLiters) {
+        this.engineDisplacementInLiters = engineDisplacementInLiters;
+    }
+
+    /**
+     * @methodtype get
+     * @return Total displacement of the car'S engine.
+     */
+    public float getEngineDisplacementInLiters() {
+        return this.engineDisplacementInLiters;
+    }
+
+    /**
+     * @methodtype set
+     * @param carLength
+     */
+    public void setCarLength(float carLength) {
+        this.carLength = carLength;
+    }
+
+    /**
+     * @methodtype get
+     * @return Length of the car.
+     */
+    public float getCarLength() {
+        return this.carLength;
+    }
+
+    /**
+     * @methodtype set
+     * @param carWidth
+     */
+    public void setCarWidth(float carWidth) {
+        this.carWidth = carWidth;
+    }
+
+    /**
+     * @methodtype get
+     * @return Width of the car.
+     */
+    public float getCarWidth() {
+        return this.carWidth;
+    }
+}
