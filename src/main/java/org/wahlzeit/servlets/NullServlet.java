5a2da0e7b823a18463b2408421bc16d37336283a
Jan Niklas Bauer <jan.niklas.bauer@fau.de>
2017-01-08 21:43:03 +0100

    added annotation for NullObject pattern

--- src/main/java/org/wahlzeit/servlets/NullServlet.java
+++ src/main/java/org/wahlzeit/servlets/NullServlet.java
@@ -20,6 +20,8 @@
 
 package org.wahlzeit.servlets;
 
+import org.wahlzeit.utils.DesignPattern;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,6 +31,10 @@ import java.io.IOException;
 /**
  * A null servlet.
  */
+@DesignPattern(
+		name = "NullObject",
+		participants = {"NullObject"}
+)
 public class NullServlet extends AbstractServlet {
 
 	/**
